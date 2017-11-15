9c4163f6d646a3e1386c6938eb3c33ad930729a2
GeorgiosP <papadoc7@users.noreply.github.com>
2017-11-15 17:24:02 +0000

    Create cldr.js

---
+++ cldr.js
@@ -0,0 +1,139 @@
+/*global  Globalize*/
+
+Globalize.load({
+  'main': {
+    'en': {
+      'numbers': {
+        'defaultNumberingSystem': 'latn',
+        'symbols-numberSystem-latn': {
+          'decimal': '.',
+          'group': ',',
+          'list': ';',
+          'percentSign': '%',
+          'plusSign': '+',
+          'minusSign': '-',
+          'exponential': 'E',
+          'superscriptingExponent': '×',
+          'perMille': '‰',
+          'infinity': '∞',
+          'nan': 'NaN',
+          'timeSeparator': ':'
+        },
+        'decimalFormats-numberSystem-latn': {
+          'standard': '#,##0.###',
+        },
+      }
+    },
+    'en-GB': {
+      'numbers': {
+        'defaultNumberingSystem': 'latn',
+        'symbols-numberSystem-latn': {
+          'decimal': '.',
+          'group': ',',
+          'list': ';',
+          'percentSign': '%',
+          'plusSign': '+',
+          'minusSign': '-',
+          'exponential': 'E',
+          'superscriptingExponent': '×',
+          'perMille': '‰',
+          'infinity': '∞',
+          'nan': 'NaN',
+          'timeSeparator': ':'
+        },
+        'decimalFormats-numberSystem-latn': {
+          'standard': '#,##0.###',
+        },
+      }
+    },
+    'pt': {
+      'numbers': {
+        'defaultNumberingSystem': 'latn',
+        'symbols-numberSystem-latn': {
+          'decimal': ',',
+          'group': '.',
+          'list': ';',
+          'percentSign': '%',
+          'plusSign': '+',
+          'minusSign': '-',
+          'exponential': 'E',
+          'superscriptingExponent': '×',
+          'perMille': '‰',
+          'infinity': '∞',
+          'nan': 'NaN',
+          'timeSeparator': ':'
+        },
+        'decimalFormats-numberSystem-latn': {
+          'standard': '#,##0.###',
+        },
+      }
+    },
+    'fr': {
+      'numbers': {
+        'defaultNumberingSystem': 'latn',
+        'symbols-numberSystem-latn': {
+          'decimal': ',',
+          'group': ' ',
+          'list': ';',
+          'percentSign': '%',
+          'plusSign': '+',
+          'minusSign': '-',
+          'exponential': 'E',
+          'superscriptingExponent': '×',
+          'perMille': '‰',
+          'infinity': '∞',
+          'nan': 'NaN',
+          'timeSeparator': ':'
+        },
+        'decimalFormats-numberSystem-latn': {
+          'standard': '#,##0.###',
+        },
+      }
+    },
+    'es': {
+      'numbers': {
+        'defaultNumberingSystem': 'latn',
+        'minimumGroupingDigits': '2',
+        'symbols-numberSystem-latn': {
+          'decimal': ',',
+          'group': '.',
+          'list': ';',
+          'percentSign': '%',
+          'plusSign': '+',
+          'minusSign': '-',
+          'exponential': 'E',
+          'superscriptingExponent': '×',
+          'perMille': '‰',
+          'infinity': '∞',
+          'nan': 'NaN',
+          'timeSeparator': ':'
+        },
+        'decimalFormats-numberSystem-latn': {
+          'standard': '#,##0.###',
+        },
+      }
+    },
+    'de': {
+      'numbers': {
+        'defaultNumberingSystem': 'latn',
+        'symbols-numberSystem-latn': {
+          'decimal': ',',
+          'group': '.',
+          'list': ';',
+          'percentSign': '%',
+          'plusSign': '+',
+          'minusSign': '-',
+          'exponential': 'E',
+          'superscriptingExponent': '·',
+          'perMille': '‰',
+          'infinity': '∞',
+          'nan': 'NaN',
+          'timeSeparator': ':'
+        },
+        'decimalFormats-numberSystem-latn': {
+          'standard': '#,##0.###',
+        },
+      }
+    }
+  }
+});
