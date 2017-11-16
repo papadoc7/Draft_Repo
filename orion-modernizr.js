d524f0a097909b2be3052b7a634ee16954d466d8
GeorgiosP <papadoc7@users.noreply.github.com>
2017-11-16 17:19:23 +0000

    Update orion-modernizr.js

--- orion-modernizr.js
+++ orion-modernizr.js
@@ -1,2 +1,2 @@
-alert("modernizr")
+alert("modernizr" + modernizr)
 var Modernizr={};Modernizr.input={placeholder:!0},Modernizr.svg=!0,Modernizr.inlinesvg=!0;var root=document.documentElement;"ontouchstart"in window||window.DocumentTouch&&document instanceof window.DocumentTouch?root.classList.add("touch"):root.classList.add("no-touch");
